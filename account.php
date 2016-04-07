f98ec1523dbe5fb983d5aab33abcd949def655e7
Jacob Nash <jpnash@mail.bradley.edu>
2016-04-06 19:17:04 -0500

    Initial Commit

---
+++ account.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+
+    <title>Account: [Insert Name]</title>
+
+    
+
+    <!-- Custom styles -->
+    <!--<link href="commonstyles.css" rel="stylesheet" type="text/css">-->
+   
+</head>
+<body>
+
+
+
+
+
+
+	<?php
+        require_once("bootstrap-body.php");
+    ?>
+</body>
---
+++ bootstrap-head.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap core CSS -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/dropdown.css" rel="stylesheet">
+<script src="bootstrap/assets/js/ie-emulation-modes-warning.js"></script>
