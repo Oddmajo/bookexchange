d63e3ea9c450489382e07b0f9df1698f5523df4d
Jacob Nash <jpnash@mail.bradley.edu>
2016-04-13 18:31:21 -0500

    Basic Format & Navbar
    Basic format of the account page, inclusion of the navbar.php file

--- account.php
+++ account.php
@@ -7,14 +7,88 @@
 
     <title>Account: [Insert Name]</title>
 
-    
+    <?php
+        require_once("bootstrap-head.php");
+    ?>
 
     <!-- Custom styles -->
     <!--<link href="commonstyles.css" rel="stylesheet" type="text/css">-->
    
 </head>
 <body>
+<?php
+	include_once("navbar.php");
+?>
+<div class="container" style="margin-top:100px;">
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<div class="col-lg-6">
+				Name:<br>
+				Phone Number:<br>
+				Email:<br>
+				<hr>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Shipping Address
+					</div>
+					<div class="panel-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				School:<br>
+				Major:<br>
+				[Placeholder]:<br>
+				<hr>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Shipping Address
+					</div>
+					<div class="panel-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<div class="col-lg-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">Need</div>
+			<div class="panel-body">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">Have</div>
+			<div class="panel-body">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
+</div>
 
 
 
---
+++ navbar.php
@@ -0,0 +1,32 @@
+<head>
+    <!--<link href="navbar.css" rel="stylesheet" type="text/css">-->
+    <link href='http://fonts.googleapis.com/css?family=Russo+One|Pathway+Gothic+One' rel='stylesheet' type='text/css'>
+</head>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Book Exchange</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="forum.php">In-Demand</a></li>
+                <li class="dropdown">
+                    <a href="about.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="five-tool.php">More About Us</a></li>
+                    </ul>
+				</li>
+				<li><a href="login.php">Sign In</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
